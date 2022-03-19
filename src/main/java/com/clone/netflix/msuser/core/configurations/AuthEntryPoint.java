642bb00a0a65634689b9a8247aa3e02913e6e765
muskaplan <muskaplan@innova.com.tr>
2022-03-19 20:21:37 +0300

    add security configurations

---
+++ src/main/java/com/clone/netflix/msuser/core/configurations/AuthEntryPoint.java
@@ -0,0 +1,16 @@
+package com.clone.netflix.msuser.core.configurations;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class AuthEntryPoint implements AuthenticationEntryPoint {
+
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
+        response.sendError(HttpStatus.UNAUTHORIZED.value(), HttpStatus.UNAUTHORIZED.getReasonPhrase());
+    }
+}
